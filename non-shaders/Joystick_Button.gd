1a886fa6ab9d2f1aac3b3896433349e0f54b9951
Gonkee <41216697+Gonkee@users.noreply.github.com>
2019-05-05 09:47:07 +1000

    joystick

---
+++ non-shaders/Joystick_Button.gd
@@ -0,0 +1,42 @@
+extends TouchScreenButton
+
+# Gonkee's joystick script for Godot 3 - full tutorial https://youtu.be/uGyEP2LUFPg
+# If you use this script, I would prefer if you gave credit to me and my channel
+
+# Change these based on the size of your button and outer sprite
+var radius = Vector2(32, 32)
+var boundary = 64
+
+var ongoing_drag = -1
+
+var return_accel = 20
+
+var threshold = 10
+
+func _process(delta):
+	if ongoing_drag == -1:
+		var pos_difference = (Vector2(0, 0) - radius) - position
+		position += pos_difference * return_accel * delta
+
+func get_button_pos():
+	return position + radius
+
+func _input(event):
+	if event is InputEventScreenDrag or (event is InputEventScreenTouch and event.is_pressed()):
+		var event_dist_from_centre = (event.position - get_parent().global_position).length()
+
+		if event_dist_from_centre <= boundary * global_scale.x or event.get_index() == ongoing_drag:
+			set_global_position(event.position - radius * global_scale)
+
+			if get_button_pos().length() > boundary:
+				set_position( get_button_pos().normalized() * boundary - radius)
+
+			ongoing_drag = event.get_index()
+
+	if event is InputEventScreenTouch and !event.is_pressed() and event.get_index() == ongoing_drag:
+		ongoing_drag = -1
+
+func get_value():
+	if get_button_pos().length() > threshold:
+		return get_button_pos().normalized()
+	return Vector2(0, 0)
